ba7a76080291690160783ea1c4c2bcaa440b2402
lalavila <wangosky@gmail.com>
2020-11-27 18:26:02 +0800

    无

--- pages/talking/index.vue
+++ pages/talking/index.vue
@@ -1,42 +1,48 @@
 <template>
     <div class="container">
-        <nuxt-link
-            v-for="item in result"
-            :key="item.id"
-            class="item hover"
-            :to="{ name: 'talking-id', params: { id: item.id } }"
-        >
-            <el-row type="flex">
-                <div class="detail">
-                    <div>
-                        <h2 class="title">{{ item.title }}</h2>
-                        <p class="shortcut">{{ item.shortcut }}</p>
+        <div class="talkings">
+            <nuxt-link
+                v-for="item in result"
+                :key="item.id"
+                class="item hover"
+                :to="{ name: 'talking-id', params: { id: item.id } }"
+            >
+                <el-row type="flex">
+                    <div class="detail">
+                        <div>
+                            <h2 class="title">{{ item.title }}</h2>
+                            <p class="shortcut">{{ item.shortcut }}</p>
+                        </div>
+                        <div class="info">
+                            <span class="right5">发布于</span>
+                            <span class="right15">{{
+                                $common.beautifulTime(
+                                    item.publish_time_timestamp,
+                                )
+                            }}</span>
+                            <span class="right5">阅读需</span>
+                            <span>
+                                {{ Math.floor(item.length / 300) }}分钟
+                            </span>
+                        </div>
                     </div>
-                    <div class="info">
-                        <span class="right5">发布于</span>
-                        <span class="right15">{{
-                            $common.beautifulTime(item.publish_time_timestamp)
-                        }}</span>
-                        <span class="right5">阅读需</span>
-                        <span> {{ Math.floor(item.length / 300) }}分钟 </span>
-                    </div>
-                </div>
-                <el-image
-                    v-if="item.thumbnail_url"
-                    class="thumbnail"
-                    fit="cover"
-                    :src="
-                        $common.formatImgUrl({
-                            url: item.thumbnail_url,
-                            width: 400,
-                            height: 200,
-                        })
-                    "
-                />
-            </el-row>
-        </nuxt-link>
-        <i v-if="loding" class="el-icon-loading loading"></i>
-        <p v-if="nomore" class="nomore">没有更多啦~~</p>
+                    <el-image
+                        v-if="item.thumbnail_url"
+                        class="thumbnail"
+                        fit="cover"
+                        :src="
+                            $common.formatImgUrl({
+                                url: item.thumbnail_url,
+                                width: 400,
+                                height: 200,
+                            })
+                        "
+                    />
+                </el-row>
+            </nuxt-link>
+            <i v-if="loding" class="el-icon-loading loading"></i>
+            <p v-if="nomore" class="nomore">没有更多啦~~</p>
+        </div>
     </div>
 </template>
 
@@ -119,60 +125,61 @@ export default {
     margin 0 auto
     padding 0 25px
     padding-top 100px
-    margin -15px
     @media only screen and (max-width: 767px)
         padding-top 40px
-    .loading, .nomore
-        display block
-        font-size 36px
-        text-align center
-        margin 60px auto
-        color $weak-color
-    .nomore
-        font-size 14px
-    .loading
-        font-size 36px
-    .item
-        // margin-bottom 30px
-        padding 15px
-        .thumbnail
-            width 300px
-            height 168.75px
-            margin-left 20px
-            border-radius 4px
-            @media only screen and (max-width: 767px)
-                width 180px
-                height 112.5px
-                margin-right 14px
-        .detail
-            flex 1
-            display flex
-            flex-direction column
-            justify-content space-between
-            .info
-                font-size 14px
-                color $prompt-color
+    .talkings
+        margin -15px
+        .loading, .nomore
+            display block
+            font-size 36px
+            text-align center
+            margin 60px auto
+            color $weak-color
+        .nomore
+            font-size 14px
+        .loading
+            font-size 36px
+        .item
+            // margin-bottom 30px
+            padding 15px
+            .thumbnail
+                width 300px
+                height 168.75px
+                margin-left 20px
+                border-radius 4px
                 @media only screen and (max-width: 767px)
-                    font-size 12px
-            .title
-                font-weight 700
-                line-height 1.4
-                margin-bottom 6px
-                overflow hidden
-                text-overflow ellipsis
-                display -webkit-box
-                -webkit-line-clamp 2
-                -webkit-box-orient vertical
-                @media only screen and (max-width: 767px)
-                    font-size 16px
-                    margin-bottom 4px
-            .shortcut
-                overflow hidden
-                text-overflow ellipsis
-                display -webkit-box
-                -webkit-line-clamp 2
-                -webkit-box-orient vertical
-                @media only screen and (max-width: 767px)
+                    width 180px
+                    height 112.5px
+                    margin-right 14px
+            .detail
+                flex 1
+                display flex
+                flex-direction column
+                justify-content space-between
+                .info
                     font-size 14px
-                    -webkit-line-clamp 1
+                    color $prompt-color
+                    @media only screen and (max-width: 767px)
+                        font-size 12px
+                .title
+                    font-weight 700
+                    line-height 1.4
+                    margin-bottom 6px
+                    overflow hidden
+                    text-overflow ellipsis
+                    display -webkit-box
+                    -webkit-line-clamp 2
+                    -webkit-box-orient vertical
+                    @media only screen and (max-width: 767px)
+                        font-size 16px
+                        margin-bottom 4px
+                .shortcut
+                    overflow hidden
+                    text-overflow ellipsis
+                    display -webkit-box
+                    -webkit-line-clamp 2
+                    -webkit-box-orient vertical
+                    @media only screen and (max-width: 767px)
+                        font-size 14px
+                        -webkit-line-clamp 1
 </style>
