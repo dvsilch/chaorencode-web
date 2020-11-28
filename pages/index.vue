ea92be811919c915139ebd64a648c05026018419
lalavila <wangosky@gmail.com>
2020-11-28 20:21:43 +0800

    无

--- pages/index.vue
+++ pages/index.vue
@@ -52,13 +52,13 @@
                             {{ item.shortcut }}
                         </p> -->
                             <div class="info">
-                                <span class="right5">发布于</span>
+                                <span class="right5">发布</span>
                                 <span class="right15">{{
                                     $common.beautifulTime(
                                         item.publish_time_timestamp,
                                     )
                                 }}</span>
-                                <span class="right5">阅读需</span>
+                                <span class="right5">阅读</span>
                                 <span>
                                     {{ Math.floor(item.length / 300) }}分钟
                                 </span>
