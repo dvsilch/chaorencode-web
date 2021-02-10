454bcf0f2aca6d36d8d07814bdcdd669a6de8d18
lalavila <wangosky@gmail.com>
2021-02-11 06:32:16 +0800

    无

---
+++ layouts/none.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="main">
+        <Nuxt />
+    </div>
+</template>
+
+<style lang="stylus" scoped></style>
---
+++ pages/red.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="box maxwidth">
+        <el-image class="gzh" src="/gzh.jpg" fit="contain" />
+        <span>关注公众号并回复：</span>
+        <span>红包{{ $route.query.helped }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    layout: 'none',
+    async asyncData({ app }) {
+        const page = 1
+        const limit = 3
+        let talkingResult = app.$guy.get('/talkings', { data: { page, limit } })
+        let carouselsResult = app.$guy.get('/carousels')
+        let coursesResult = app.$guy.get('/courses')
+
+        carouselsResult = await carouselsResult
+        coursesResult = await coursesResult
+        talkingResult = await talkingResult
+
+        let talkings = []
+        if (talkingResult.status === 200) {
+            talkings = talkingResult.data.result
+            // talkings = talkings.concat(talkings)
+            // talkings = talkings.concat(talkings)
+            // talkings = talkings.slice(0, 3)
+        }
+
+        let carousels = []
+        if (carouselsResult.status === 200) {
+            carousels = carouselsResult.data.result
+        }
+
+        let courses = []
+        if (coursesResult.status === 200) {
+            courses = coursesResult.data.result
+        }
+
+        return {
+            courses,
+            carousels,
+            talkings,
+        }
+    },
+    head() {
+        return {
+            title: '超人编程',
+        }
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+// .box
+</style>
