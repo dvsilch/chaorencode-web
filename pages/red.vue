4474d87ce5d4991c149c4ab6b8d54814f1ba1c3e
lalavila <wangosky@gmail.com>
2021-02-11 17:40:00 +0800

    无

--- pages/red.vue
+++ pages/red.vue
@@ -1,57 +1,51 @@
 <template>
     <div class="box maxwidth">
-        <el-image class="gzh" src="/gzh.jpg" fit="contain" />
-        <span>关注公众号并回复：</span>
-        <span>红包{{ $route.query.behelp }}</span>
+        <el-image class="gzh" src="/logo.png" fit="contain" />
+        <div class="info">
+            <span>关注公众号超人编程并回复：</span>
+            <span class="red">助力{{ $route.query.behelp }}</span>
+        </div>
+        <el-button type="primary" @click="open">一键复制并打开公众号</el-button>
     </div>
 </template>
 
 <script lang="ts">
 export default {
     layout: 'none',
-    async asyncData({ app }) {
-        const page = 1
-        const limit = 3
-        let talkingResult = app.$guy.get('/talkings', { data: { page, limit } })
-        let carouselsResult = app.$guy.get('/carousels')
-        let coursesResult = app.$guy.get('/courses')
-
-        carouselsResult = await carouselsResult
-        coursesResult = await coursesResult
-        talkingResult = await talkingResult
-
-        let talkings = []
-        if (talkingResult.status === 200) {
-            talkings = talkingResult.data.result
-            // talkings = talkings.concat(talkings)
-            // talkings = talkings.concat(talkings)
-            // talkings = talkings.slice(0, 3)
-        }
-
-        let carousels = []
-        if (carouselsResult.status === 200) {
-            carousels = carouselsResult.data.result
-        }
-
-        let courses = []
-        if (coursesResult.status === 200) {
-            courses = coursesResult.data.result
-        }
-
-        return {
-            courses,
-            carousels,
-            talkings,
-        }
-    },
     head() {
         return {
             title: '超人编程',
         }
     },
+    methods: {
+        open() {
+            const el = document.createElement('textarea')
+            el.value = `助力${this.$route.query.behelp}`
+            document.body.appendChild(el)
+            el.select()
+            document.execCommand('copy')
+            document.body.removeChild(el)
+
+            window.location.href = 'http://weixin.qq.com/r/SC6EnJ3E38Cgrdn-93ta'
+        },
+    },
 }
 </script>
 
 <style lang="stylus" scoped>
-// .box
+.box
+    display flex
+    flex-direction column
+    align-items center
+    margin-top 20vh
+    .gzh
+        width 180px
+        height 180px
+        border-radius 90px
+        border 14px solid #d5dfe8
+        margin-bottom 10px
+    .info
+        margin-bottom 10px
+        .red
+            color red
 </style>
