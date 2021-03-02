532e29979977ed9b4bc85f0b557ef1bcc63f1968
lalavila <wangosky@gmail.com>
2021-03-02 22:20:14 +0800

    无

---
+++ pages/_id.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container"></div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+    mounted() {
+        alert(this.$route.params.id)
+    },
+})
+</script>
